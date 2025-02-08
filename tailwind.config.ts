df3ebda1f78311f6134f8b8c7dd0570c28090621
Youngjju <2000dudwn@naver.com>
2025-02-09 02:58:56 +0900

    chore: main page progressing

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        pretendard: ["Pretendard", "sans-serif"], // Pretendard를 기본 폰트로 설정
+      },
     },
   },
   plugins: [],
