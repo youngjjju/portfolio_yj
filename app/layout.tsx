f5271f731747ec4843b083e6e87c0401cf67f42c
Youngjju <2000dudwn@naver.com>
2025-02-16 21:56:46 +0900

    chore: reset all

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,17 +7,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <head>
-        {/* Pretendard 폰트 추가 */}
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/@fontsource/pretendard/css/pretendard.css"
-        />
-      </head>
-      <body className="bg-[rgb(255,255,255)] font- font-pretendard">
-        {/* 전체에 Pretendard 폰트 적용 */}
-        {children}
-      </body>
+      <head></head>
+      <body>{children}</body>
     </html>
   );
 }
