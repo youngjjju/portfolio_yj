b59b03de3d0aea7c08b4dde04055840bc97e7f2f
Youngjju <2000dudwn@naver.com>
2025-03-05 03:22:24 +0900

    chore: remove outdated backend skills description from Skills component

--- components/project/skills.tsx
+++ components/project/skills.tsx
@@ -18,12 +18,6 @@ export default function Skills() {
             웹 개발부터 회로설계 언어까지 다양한 기술을 활용해왔습니다. HTML,
             CSS, JavaScript, React, Next.js, TypeScript로 직관적이고 반응형 UI를
             구현하고, Tailwind CSS로 스타일링을 빠르고 일관성 있게 처리했습니다.
-          </p>
-          <p>
-            백엔드에서는 Django로 REST API를 구축하며 데이터베이스 연동과
-            비즈니스 로직을 처리했으며, Node.js로 비동기 처리를 최적화했습니다.
-          </p>
-          <p>
             또한 C와 Verilog를 사용해 저수준 프로그래밍과 하드웨어 제어 경험을
             쌓았고, Git과 Github로 체계적인 버전 관리와 협업을 진행했습니다.
           </p>
